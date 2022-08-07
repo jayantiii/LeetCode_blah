e6b1ed72e48445c991c9a2745e3931d6349856fa
lalalalalala <66568334+jayantiii@users.noreply.github.com>
2022-08-07 23:25:00 +0530

    Time: 171 ms (34.91%), Space: 13.8 MB (82.76%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,26 +1,32 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool: # O(9*9)
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+        
 
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".":
+        # hash set for each row in the grid
+        # hash set for each col in the grid
+        # hash set for each 3x3 box in grid
+        row = collections.defaultdict(set)
+        col = collections.defaultdict(set)
+        box = collections.defaultdict(set)
+        
+    # in the first 2 dict , we keep row and col no. as key and values will be a set of numbers.
+    #in last dict, 
+        for i in range(9):
+            for j in range(9):
+                
+                if board[i][j] == ".": 
                     continue
-                if (
-                    board[r][c] in rows[r]
-                    or board[r][c] in cols[c]
-                    or board[r][c] in squares[(r // 3, c // 3)]
+                if(
+                   board[i][j] in row[i] or
+                   board[i][j] in col[j] or
+                   board[i][j] in box[(i//3,j//3)]
                 ):
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
 
+                row[i].add(board[i][j])
+                col[j].add(board[i][j])
+                box[(i//3,j//3)].add(board[i][j])
         return True
-
-
                                                                
                     
                    
