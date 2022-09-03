35c89f4062de6421d4ec6e89141dcce06ec0c4d4
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2022-09-03 20:23:27 +0530

    Time: 67 ms (16.94%), Space: 13.9 MB (64.57%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -3,5 +3,30 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        # single line -->  nums.sort()   --># haha but we need to solve without this.
+        
+        #idea - 2 pointers ( quicksort) - see notes
+        l,r = 0, len(nums)-1
+        i = 0
+        def swap(i,j):
+            temp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = temp
+            
+        while i <= r: # condition 
+            
+            if nums[i] == 0:
+                swap(l,i)
+                l+=1
+            
+            elif nums[i] == 2:
+                swap(r,i)
+                r -=1
+                i-=1
+            i+=1
+        return nums
+            
+                
+        
+            
         
