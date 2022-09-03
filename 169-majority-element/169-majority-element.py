a830e49045af545fd04ba71871c13464a8780d3b
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2022-09-03 16:00:31 +0530

    Time: 332 ms (15.59%), Space: 15.5 MB (85.91%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,22 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        #linear time and 0(1) space
-        count = {}
-        maxcount,res = 0,0
+        #linear time and 0(1) space -- boyer moore algo
+        # kinda confusing
+        res,count = 0,0
         for n in nums:
-            count[n] = count.get(n,0)+1
-            res = n if count[n] > maxcount else res
-            maxcount = max(count[n],maxcount)
+            if count == 0:
+                res = n
+            count += (1 if n==res else -1)
         return res
+          
+            
        
-
-
-
-
-
-
-
-
+        
+       
         # #my soln
         # l = len(nums)
         # dic = {}
@@ -26,4 +22,13 @@ class Solution:
         #     if v > l/2:
         #         return k
         
+        
+        # another soln
+        # count = {} 
+        # maxcount,res = 0,0
+        # for n in nums:
+        #     count[n] = count.get(n,0)+1
+        #     res = n if count[n] > maxcount else res
+        #     maxcount = max(count[n],maxcount)
+        # return res
         
