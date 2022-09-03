219590ca41db4a65f39f855df0870d50762b3273
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2022-09-03 10:01:03 +0530

    Time: 216 ms (71.97%), Space: 15.6 MB (34.32%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,11 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        l = len(nums)
-        dic = {}
+        #linear time and 0(1) space
+        count = {}
+        maxcount,res = 0,0
         for n in nums:
-            dic[n] = dic.get(n,0) +1
-        for k,v in dic.items():
-            if v > l/2:
-                return k
+            count[n] = count.get(n,0)+1
+            res = n if count[n] > maxcount else res
+            maxcount = max(count[n],maxcount)
+        return res
+       
+
+
+
+
+
+
+
+
+        # #my soln
+        # l = len(nums)
+        # dic = {}
+        # for n in nums:
+        #     dic[n] = dic.get(n,0) +1
+        # for k,v in dic.items():
+        #     if v > l/2:
+        #         return k
         
         
