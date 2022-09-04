18e866b9ca85f5f9ccd042d5c405d8220148855c
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2022-09-04 17:34:21 +0530

    Time: 43 ms (79.44%), Space: 13.9 MB (29.37%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,11 +1,23 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        #observe the pattern in input and output matrix
+    
+        #Do not return anything, modify matrix in-place instead.
+        '''observe the pattern in input and output matrix, this logic hard, see notes video '''
+        l,r = 0, len(matrix) -1
+        while l<r :
+            for i in range(r-l):
+                t,b = l,r          #top bottom left right pointers
+                topleft_temp = matrix[t][l+i] # save topleft value
+                matrix[t][l+i] = matrix[b-i][l] #move bl to tl
+                matrix[b-i][l] = matrix[b][r-i] #move br to bl
+                matrix[b][r-i] = matrix[t+i][r] # move tr to br
+                matrix[t+i][r] = topleft_temp  #move tl to tr
+            r-=1
+            l+=1
+            
+''' simpler method  --> transpose then reverse           
         n = len(matrix)
-        for i in range(n):
+        for i in range(n): # transpose
             for j in range(i,n): # use (i,n) not (n) to prevent overwriting - tricky
                 # we need to use temp here cause we are replacing in the same 2d array
                 temp = matrix[i][j]
@@ -14,6 +26,6 @@ class Solution:
         
         for row in matrix:
             row.reverse()
-        matrix = list(map(lambda x: x[::-1],matrix))
-        
+        # matrix = list(map(lambda x: x[::-1],matrix)) -- a method to reverse
+ '''       
         
