5b9768a32bfe2310cbe61f799003a2fc7839ca71
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2024-12-31 17:43:50 -0800

    Time: 16 ms (27.28%) | Memory: 17.8 MB (28.67%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -8,10 +8,21 @@ class Solution:
             scount[s[i]] = 1 + scount.get(s[i],0) 
             tcount[t[i]] = 1 + tcount.get(t[i],0) 
         return scount == tcount
-
-# return sorted(s) == sorted(t)   - O(nlogn+mlogm)  
-
-
+        
 #anagram is not equal to palindrome (dont confuse)
+# return sorted(s) == sorted(t)   - O(nlogn+mlogm) 
+# return counter(s) == counter(t) - similar efficency as submitted soln
+
+#more optimal way
+#  count = [0] * 26
+#         for i in range(len(s)):
+#             count[ord(s[i]) - ord('a')] += 1
+#             count[ord(t[i]) - ord('a')] -= 1
+
+#         for val in count:
+#             if val != 0:
+#                 return False
+#         return True
+
         
         
