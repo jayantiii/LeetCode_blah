83729bd56bcce5493448c8eab090710acd072b60
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-01 17:19:01 -0800

    Time: 3 ms (98.74%) | Memory: 19.4 MB (18.64%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,11 +1,22 @@
 import re
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        #to be efficient, use two pointer algo
         s = s.lower().strip()
         ss = re.sub(r'[^a-zA-Z0-9]','',s)
-        print(ss)
-
         return ss == ss[::-1]
 
-#filtered = ''.join(c for c in s if c.isalnum())
+# 2 pointer way, to be efficient, use two pointer algo
+    # i, j = 0, len(s) - 1
+    #     while i < j:
+    #         while i < j and not s[i].isalnum(): i += 1
+    #         while i < j and not s[j].isalnum(): j -= 1
+    #         if s[i].lower() != s[j].lower(): return False
+    #         i += 1
+    #         j -= 1
+    #     return True
+
+#bitwise not operator
+    #  s = [c.lower() for c in s if c.isalnum()]
+    #     return all (s[i] == s[~i] for i in range(len(s)//2))
+    
+#filtered = ''.join(c for c in s if c.isalnum()) - one other way
