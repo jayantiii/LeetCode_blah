45b84a02a96d5f3878d1c718cb214110a487aec9
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-01 16:15:24 -0800

    Time: 15 ms (50.65%) | Memory: 20.7 MB (49.77%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,6 +1,7 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-
+        # tc = (m*nlogn) where n is length of longest string
+        #sc = O(m*n)
         res = defaultdict(list)
         for s in strs:
             sortedw = ''.join(sorted(s)) #sorted makes it array like [a,e,t]
@@ -8,6 +9,8 @@ class Solution:
             res[sortedw].append(s)
 
         return list(res.values())
+
+#hash - O(mxn) , O(m), m is no. of strings
         # res = defaultdict(list)
         # for word in strs:
         #     count = [0] *  26
