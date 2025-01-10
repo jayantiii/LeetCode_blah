73b0cabacce970478e2f23857da721d01167622a
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-10 00:00:05 -0800

    Time: 14 ms (12.76%) | Memory: 24.1 MB (8.29%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,16 +1,34 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        dic = {}
+    #use a modified version of bubble sort - 0(n),o(n)
+        count = {}
+        freq = [[] for i in range (len(nums) + 1)]
         res = []
         for i in range(len(nums)):
-            dic[nums[i]] = dic.get(nums[i], 0) + 1
-        for key,val in dic.items():
-            res.append([val,key])
-        res.sort()
-        ans =[]
-        while len(ans) < k:
-            ans.append(res.pop()[1])
-        return ans
+            count[nums[i]] = count.get(nums[i],0) + 1
+        for key,val in count.items():
+            freq[val].append(key)
+        for i in range(len(freq) -1,0,-1):
+            for num in freq[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
 
+    
+
+
+
+# naive - O(nlogn) ,
+        # dic = {}
+        # res = []
+        # for i in range(len(nums)):
+        #     dic[nums[i]] = dic.get(nums[i], 0) + 1
+        # for key,val in dic.items():
+        #     res.append([val,key])
+        # res.sort()
+        # ans =[]
+        # while len(ans) < k:
+        #     ans.append(res.pop()[1])
+        # return ans
 
 
