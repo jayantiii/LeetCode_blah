8a6f3413894284768683e5d3380d67a8da0491fd
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-09 16:46:41 -0800

    Time: 0 ms (100.00%) | Memory: 18.9 MB (21.61%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -8,8 +8,17 @@ class Solution:
             dic[nums[i]] = i
 
 
-# this passes only 2 cases
+# this passes only 2 cases - think issue
 # for i in range(len(nums)):
 #             a = target - nums[i]
 #             if a in nums and nums[i]!=a:   --- logic issue when nums = [3,3]
 #                 return [i, nums.index(a)]
+
+
+#brute
+#  for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i, j]
+
+#another way(not efficient) -> sort and use 2 pointer
