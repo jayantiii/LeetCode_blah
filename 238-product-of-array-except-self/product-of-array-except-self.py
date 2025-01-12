23624c324be48dd3045775ef04ca771fee4c3202
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-11 20:04:07 -0800

    Time: 23 ms (65.33%) | Memory: 23.3 MB (58.04%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,5 +1,6 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        #O(n),0(1)
         prod = 1
         res = [0]*len(nums) 
         zeroc = 0
@@ -18,6 +19,32 @@ class Solution:
                 res[i] = prod//nums[i]
         return res
 
+#optimal way - have one prefix and one suffix product arrays ( should try)
+#   res = [1] * (len(nums))
+#         prefix = 1
+#         for i in range(len(nums)):
+#             res[i] = prefix
+#             prefix *= nums[i]
+#         postfix = 1
+#         for i in range(len(nums) - 1, -1, -1):
+#             res[i] *= postfix
+#             postfix *= nums[i]
+#         return res
+
+# same answer as submitted, just see how written from neetcode
+    #    prod, zero_cnt = 1, 0
+    #     for num in nums:
+    #         if num:             -- if nums =0 , then false!
+    #             prod *= num
+    #         else:
+    #             zero_cnt +=  1
+    #     if zero_cnt > 1: return [0] * len(nums)
+
+    #     res = [0] * len(nums)
+    #     for i, c in enumerate(nums):
+    #         if zero_cnt: res[i] = 0 if c else prod
+    #         else: res[i] = prod // c
+    #     return res
 
 
         
@@ -42,3 +69,5 @@ class Solution:
         #     else:
         #         res.append(ans//nums[i])
         # return res
+
+#brute force is to use 2 nested for loops
