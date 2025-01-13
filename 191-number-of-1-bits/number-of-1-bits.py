dc452a4ef4b71677307c77f544307efba6d90237
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-13 01:42:23 -0800

    Time: 3 ms (17.04%) | Memory: 17.9 MB (13.88%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -3,17 +3,24 @@ class Solution:
         bit = bin(n)[2:]
         res = 0
         print(bit)
+        # O(1) because max is O(32) that is constant
         for b in bit:
             if int(b) == 1:
                 res+=1
         return res
 
-        
-
-
+#another idea
+# while bit:
+#         res += bit % 2.  -- 2 modulo with 1 is 1, with zero is zero
+#         n = n >>1     -- shift right by one bit
+#      return res
+    
 # a way to convert dec to bits
 # DecimalToBinary(num):
 #         if num >= 1:
 #             DecimalToBinary(num // 2)
 #            print num % 2 
+
 #another way - bin(n).replace("0b", "") or bin(4785)[2:]
+
+#one line - return bin(n).count('1')
