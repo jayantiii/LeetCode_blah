4be76ef03b99722fb0d3399e81c7e099b17e4754
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-15 04:01:58 -0800

    Time: 0 ms (100.00%) | Memory: 29.2 MB (8.92%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -24,6 +24,28 @@ class Solution:
 #     print("here")
 #     return False
             
+##neetcode ans
+#  visit = set()  # MEMO to track visited numbers
+
+#     while n not in visit:
+#         visit.add(n)
+#         n = self.sumOfSquares(n)
+
+#         if n == 1:
+#             return True
+
+#     return False
+
+# def sumOfSquares(self, n: int) -> int:
+#     output = 0
+
+#     while n:
+#         digit = n % 10
+#         digit = digit ** 2
+#         output += digit
+#         n = n // 10
+
+#     return output
 
 
 
