fcd7b11bfbfe37315675d0ad534ee47a26e118a6
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-01-15 16:15:00 -0800

    Time: 41 ms (35.86%) | Memory: 17.9 MB (7.77%) - LeetSync

--- 190-reverse-bits/reverse-bits.py
+++ 190-reverse-bits/reverse-bits.py
@@ -1,5 +1,6 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        #bin(n)[2:] removes the 0b prefix but only includes the significant digits of the binary number.
         s = str(bin(n)[2:]).zfill(32)
         print(s, s[::-1])
         return int(s[::-1],2)
