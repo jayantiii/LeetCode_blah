5b162d76948343719d061daf4b6dedeb1a7fad46
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-02-15 04:11:38 -0800

    Time: 0 ms (100.00%) | Memory: 18.9 MB (6.44%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+# so basically, use l and r and find mid each iteration
+        l, r = 0, len(nums) - 1 #dont forget -1 for r, and l is o not 1
+        while l <=r:
+            mid = (l + r )// 2
+            if nums[mid] == target:
+                return mid
+            else:
+                if nums[mid] > target:
+                    r = mid -1
+                else:
+                    l = mid + 1
+        return -1
+
+        
