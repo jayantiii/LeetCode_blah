b3f48e220a84c840d3fed4d62162762187e4f32b
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-05-03 23:08:48 -0700

    Time: 7 ms (73.54%) | Memory: 18.3 MB (14.32%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -5,8 +5,6 @@ class Solution:
             if i== len(flowerbed) -1 and x ==0  and flowerbed[i-1]==0: 
                 n = n-1
                 flowerbed[i] = 1
-
-
             elif i == 0 and x ==0 and flowerbed[i+1]==0:
                 n = n-1
                 flowerbed[i] = 1
@@ -18,5 +16,35 @@ class Solution:
             
         return n <=0   # this how u return for true or false
 
+#cleaner version of above code
+# for i, x in enumerate(flowerbed):
+#             if x == 0:
+#                 prev_empty = (i == 0) or (flowerbed[i - 1] == 0)
+#                 next_empty = (i == len(flowerbed) - 1) or (flowerbed[i + 1] == 0)
 
-        
+#                 if prev_empty and next_empty:
+#                     flowerbed[i] = 1
+#                     n -= 1
+#                     if n == 0:
+#                         return True
+#         return n <= 0
+
+
+# another wayy
+# #def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+#         i = 0
+#         while i < len(flowerbed):
+#             if flowerbed[i] == 1:
+#                 i += 2  # skip next one; cannot plant there
+#             else:
+#                 left = i == 0 or flowerbed[i - 1] == 0
+#                 right = i == len(flowerbed) - 1 or flowerbed[i + 1] == 0
+#                 if left and right:
+#                     flowerbed[i] = 1
+#                     n -= 1
+#                     if n == 0:
+#                         return True
+#                     i += 2  # skip next one (just planted)
+#                 else:
+#                     i += 1
+#         return n <= 0
