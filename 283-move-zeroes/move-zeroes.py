930d0a6fc2fee7f193b796d56ac6ec5e837610ad
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-05-04 21:30:50 -0700

    Time: 7 ms (45.34%) | Memory: 18.7 MB (79.19%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,29 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # Modify inplace!
         pos = 0
         for i in range(len(nums)): # dont forget len()
             if nums[i] != 0:
                 nums[pos] = nums[i]
                 pos+=1
-
+        
         for i in range(pos, len(nums)):
             nums[i] = 0
 
-   
-
-
-
-
-#lot of index errors
+#My start, lot of index errors
     #  zero = 0
     #     for i, x in enumerate(nums):
     #          #enumerate(nums) uses the list's original length and static index range.
     #         if x == 0:
-    #         # wrong - nums.pop(i) inside a loop over enumerate(nums), index shifting
+    #         # wrong!!! - nums.pop(i) inside a loop over enumerate(nums), index shifting
     #             zero +=1
         
-    #     # nums = nums + [0]*zero
+    #     # nums = nums + [0]*zero. --- creates a new array and also wrong index issues
     #     l = len(nums)
     #     for i in range(zero):
     #         nums[i+l] = 0
