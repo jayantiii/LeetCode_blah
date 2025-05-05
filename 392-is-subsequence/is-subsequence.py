acc20a548972292c107a20408dfb231e025b7dd6
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-05-04 21:38:30 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.77%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -3,7 +3,7 @@ class Solution:
 
         i,j = 0,0
         count = 0 # we want it to be len of s
-        while j < len(t) and i <len(s):
+        while j < len(t) and i <len(s): #  and i <len(s) important or will fail some cases
             if s[i] == t[j]:
                 count +=1
                 i+=1
@@ -13,7 +13,15 @@ class Solution:
 
         return count == len(s)
 
+# similar!
+#      sp = tp = 0
 
+        # while sp < len(s) and tp < len(t):
+        #     if s[sp] == t[tp]:
+        #         sp += 1
+        #     tp += 1
+        
+        # return sp == len(s)
                 
         
 
