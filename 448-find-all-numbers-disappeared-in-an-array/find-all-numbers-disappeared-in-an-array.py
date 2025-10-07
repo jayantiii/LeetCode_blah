c7df095dc21d0eb39bb0368c0d4e0bc1463ed366
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-10-06 18:00:59 -0700

    Time: 57 ms (8.77%) | Memory: 30.7 MB (81.51%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -1,14 +1,23 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        #whenever duplicate q, think about sets
-
-        res = []
-        set_nums = set(nums)
-
-        for i in range(1, len(nums) +1):
-            if i not in set_nums:
-                res.append(i) # this is wrong - res.append[i]
-
-        return res
-
+        arr = []
+    # Use in-place marking to get O(n) time
+        for i in range(len(nums)):
+            x = abs(nums[i])
+            nums[x-1] = -(abs(nums[x-1]))
+            #abs is necessary cause a value can repeat dont forget!!
         
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                arr.append(i+1)
+        return arr
+
+
+# ## TLE         
+#   arr = []
+#         for i in range(1,len(nums)+1):
+#             if i not in nums:
+#                 arr.append(i)
+#         return arr
+
+# The key insight is: if a number x exists in the array, we can mark its corresponding index x-1 as visited by making it negative. At the end, any index that is still positive means its number (index + 1) is missing.
