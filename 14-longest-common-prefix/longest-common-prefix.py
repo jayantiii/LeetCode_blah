7c655aead55ef458e08b7cef958d69e43a393cc7
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-14 11:18:48 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.97%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,22 +1,38 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        #make prefix first word
+        prefix = strs[0]
+        pre_len = len(prefix)
 
-        # handle empty
-        if not strs:
-            return ""
-        n = len(strs)
+        for i in range(1,len(strs)):
+            while prefix != strs[i][:pre_len]:
+                # decrease pre_len and prefix by 1 char
+                pre_len -=1
+                prefix = prefix[:pre_len]
 
-        # the trick is that this sorts lexicographically
-        # Thus, common prefix of first and last is guaranteed to be the common prefix of every string in the list.
-        strs.sort()
+        return prefix
 
-        first = strs[0]
-        last = strs[-1]
 
-        i = 0 # only one pointer needed
-        while i < len(first) and first[i] == last [i]:
-            i+=1
-        return first[:i]
+
+
+
+
+
+
+#One Another way!!
+        # # handle empty
+        # if not strs:
+        #     return ""
+        # n = len(strs)
+        # # the trick is that this sorts lexicographically
+        # # Thus, common prefix of first and last is guaranteed to be the common prefix of every string in the list.
+        # strs.sort()
+        # first = strs[0]
+        # last = strs[-1]
+        # i = 0 # only one pointer needed
+        # while i < len(first) and first[i] == last [i]:
+        #     i+=1
+        # return first[:i]
         
 
         
