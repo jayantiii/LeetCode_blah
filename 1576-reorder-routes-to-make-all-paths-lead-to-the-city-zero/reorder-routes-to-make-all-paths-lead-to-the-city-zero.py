82a20a1eb382750b4a757a5a4005f90b4c55571e
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-16 14:55:00 -0800

    Time: 182 ms (69.19%) | Memory: 55.5 MB (7.97%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -2,48 +2,50 @@ class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
         adj = { i:[] for i in range(n)} # dont use len(connections) as its no of edges
         for u,v in connections:
-            adj[u].append([v,1])
-            adj[v].append([u,0]) #reversed
-
-
-        queue = deque([])
-        queue.append(0)
-        visit = set()
-        visit.add(0) # dont forget this
+            adj[u].append([v,1]) #existing edge
+            adj[v].append([u,0]) #reverse edge
         edges = 0
-        while queue:
-            node = queue.popleft()
-            for neigh, dir in adj[node]:
-                if neigh not in visit:
-                    visit.add(neigh)
+        visit = set()
+        visit.add(0)
+        def dfs(node):
+            nonlocal edges
+                # no base condition needed here
+            for neigh, dir in adj[node]: # unpack here
+                if neigh not in visit:  #dont use return here, it then wont go to all neigh
                     edges += dir
-                    queue.append(neigh)
-
-        return edges
-
-
-        # for u,v in connections:
-        #     adj[u].append([v,1]) #existing edge
-        #     adj[v].append([u,0]) #reverse edge
-        # edges = 0
-        # visit = set()
-        # def dfs(node):
-        #     nonlocal edges
-        #         # no base condition needed here
-        #     for neigh, dir in adj[node]: # unpack here
-        #         if neigh in visit:
-        #             return
-        #         edges += dir
-        #         visit.add(neigh)
-        #         dfs(neigh)
-        #     return
+                    visit.add(neigh)
+                    dfs(neigh)
+            return
                 
 
-        # dfs(0)
-        # return edges
+        dfs(0)
+        return edges
 
   
 #Note - You built an undirected tree (each connection added twice). If you don’t track where you came from, DFS can bounce back and forth forever
+
+    #BFS WAY, works too
+        
+        # adj = { i:[] for i in range(n)} # dont use len(connections) as its no of edges
+        # for u,v in connections:
+        #     adj[u].append([v,1])
+        #     adj[v].append([u,0]) #reversed
+
+
+        # queue = deque([])
+        # queue.append(0)
+        # visit = set()
+        # visit.add(0) # dont forget this
+        # edges = 0
+        # while queue:
+        #     node = queue.popleft()
+        #     for neigh, dir in adj[node]:
+        #         if neigh not in visit:
+        #             visit.add(neigh)
+        #             edges += dir
+        #             queue.append(neigh)
+
+        # return edges
 
 
         
