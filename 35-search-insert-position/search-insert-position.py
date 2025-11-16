36fd378fc22f29f226d35813fd9ae1b396c67fe0
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-15 18:53:08 -0800

    Time: 0 ms (100.00%) | Memory: 18.3 MB (72.63%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,11 +1,28 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-            if nums[i] > target:
-                return i
+        #user binary search
+        l = 0
+        r = len(nums)-1
+        while l <= r:
+            mid = (l+r) //2
+            if nums[mid] < target:
+                l = mid +1
+                pos = l
+            elif nums[mid] > target:
+                r = mid-1
+                pos= r
+            else:
+                return mid #equal
 
-        return i+1
+        return l
+
+# this works but its O(n)
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         return i
+        #     if nums[i] > target:
+        #         return i
+
+        # return i+1 #this should be i+1 in case all nums are less than target
 
         
