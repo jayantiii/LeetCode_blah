11f229cd271339b5b5ec5f4d698f69ed533cbc58
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-16 17:29:43 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (36.19%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        #T - O(n · 2ⁿ)
         res = []
         nums.sort() # nlogn
 
@@ -15,6 +16,10 @@ class Solution:
             curr.pop()
 
             #decision2, all subsets thatbnot include nums[i]
+
+            #After you choose not to take nums[i],you skip equal elements(nums[i+1]...)
+            # If we are not including nums[i], it means we dont include any num of that value
+            #Thus, we skip the the indexs that have value nums[i], thus we sort nums before
             while i+1< len(nums) and nums[i] == nums[i+1]:
                 i+=1
             backtrack(i+1,curr)
@@ -22,4 +27,11 @@ class Solution:
         backtrack(0,[])
 
         return res
+
+
+# Brute force:
+# Generate all 2^n subsets as if all elements were distinct.
+# Normalize each subset (e.g., treat it as a tuple).
+# Use a set to remove duplicates.
+# Convert back to list of lists.
         
