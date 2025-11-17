49346c14a106cf96a11f34441a43b57f69d0253b
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-17 10:51:48 -0800

    Time: 0 ms (100.00%) | Memory: 20 MB (16.39%) - LeetSync

--- 252-meeting-rooms/meeting-rooms.py
+++ 252-meeting-rooms/meeting-rooms.py
@@ -1,19 +1,14 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        for i in range(len(intervals)):
-        ##start from i +1, if start from i, will comapre itself
-            for j in range(i+1,len(intervals)):
-                onestart, oneend = intervals[i][0] ,intervals[i][1]
+        intervals.sort(key = lambda i : i[0]) # sort on start times
 
-                twostart = intervals[j][0]
-                twoend = intervals[j][1]
-                 #max(start_i, start_j) < min(end_i, end_j) - this should be
-                if oneend > twostart and twoend > onestart:
-                    return False
+        for i in range(1, len(intervals)): # can start from 1
+            # if start time of second meet is before end of first
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
         return True
-                
-        
 
+            
 #Some clarify q's
 # For empty input (no meetings), should we return true or false?
 #If two meetings end and start at the same time, is that considered overlapping?
@@ -22,3 +17,15 @@ class Solution:
 
 #Bruetforce
 #compare every meeting time to every other meeting time
+
+        # for i in range(len(intervals)):
+        # ##IMPPPPPP !! start from i +1, if start from i, will comapre itself
+        #     for j in range(i+1,len(intervals)):
+        #         onestart, oneend = intervals[i][0] ,intervals[i][1]
+
+        #         twostart = intervals[j][0]
+        #         twoend = intervals[j][1]
+        #          #max(start_i, start_j) < min(end_i, end_j) - this should be - IMPPP
+        #         if oneend > twostart and twoend > onestart:
+        #             return False
+        # return True
