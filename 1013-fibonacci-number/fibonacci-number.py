8b08f394fa3531bca6b9b1feefb8807e0853f566
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-17 11:38:26 -0800

    Time: 365 ms (14.60%) | Memory: 17.9 MB (22.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,11 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
+        #A full binary tree of height h has: ≈ 2^h nodes
+        # Time - 2ⁿ, Space = O(n)
+        #Python does not keep all these nodes active at once. It explores one branch at a time
+        #Thus, At any moment, the deepest call chain is: f(n) → f(n-1) → f(n-2) → … → f(1)
         def fibo(n):
             if n == 0:
                 return 0
             if n == 1:
                 return 1
-            return fibo(n-1) + fibo(n-2)
+            return fibo(n-1) + fibo(n-2) #recursion tree is binary, creates two recursive calls every time.
+            #Thus Total calls ≈ 2ⁿ
 
         return fibo(n)
 
