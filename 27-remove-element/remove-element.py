3c8dfe30587f0f9b0fcaa66e7d28a10958de6c38
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-20 14:11:43 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (10.87%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -5,9 +5,6 @@ class Solution:
 
         write = len(nums) -1
         for i in range(len(nums)):
-            #Also, need a condition for checking if i < write
-            # if i>write:
-            #     break
             #But what if the next element at the new write index is also val
             #so dont do only if, do a while
             while i <= write and nums[write] == val:  #condition
@@ -15,7 +12,6 @@ class Solution:
             #Also, need a condition for checking if i < write
             if i>write:
                 break
-
             if nums[i] == val:
                 nums[i] = nums[write]
                 nums[write] = val
