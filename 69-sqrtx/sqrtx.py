9200fb71c06055ff0cf1f020422cd93cc45ded45
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-20 18:42:41 -0800

    Time: 3 ms (62.55%) | Memory: 17.9 MB (31.46%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,17 +1,33 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         #explore all int, Use the sorted property of integers to reduced the search space.
-        if x == 0:
-            return 0
-        if x == 1:
-            return 1
+        #Kind of like binary search
+        l,r = 0,x
         res = 0
-        for num in range(x):
-            if num * num <= x:
-                res = num
+        while l<=r:
+            mid = (l+r)//2
+
+            if  mid*mid > x:
+                r = mid-1
+            elif mid*mid <x:
+                l = mid +1
+                res = mid
             else:
-                return res
+                return mid
         return res
+            
+#bruteforce works!! - check all nums
+        # if x == 0:
+        #     return 0
+        # if x == 1:
+        #     return 1
+        # res = 0
+        # for num in range(x):
+        #     if num * num <= x:
+        #         res = num
+        #     else:
+        #         return res
+        # return res
 
 
         
