056d5b075733675820d012c3835c6b06ded84239
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-23 00:42:21 -0800

    Time: 0 ms (100.00%) | Memory: 19 MB (66.55%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -0,0 +1,30 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        # to be tree
+        #  - have no cycles - It is fully connected
+        #  - edges == n − 1 - DFS/BFS from node 0 visits all n nodes.
+
+        if len(edges) != n-1:return False  #Imp!
+        adj = {i:[] for i in range(n)}
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u) #both sides
+
+        visit = set()
+        def dfs(i,prev):
+            if i in visit:
+                return False
+            visit.add(i)
+            for neigh in adj[i]:
+                if neigh == prev:
+                    continue
+                dfsres = dfs(neigh,i)
+                if dfsres == False: return False 
+                #dont return if true or loop break
+
+        dfs(0,-1)
+        return len(visit) == n
+
+
+
+    
