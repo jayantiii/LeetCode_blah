452e1a5cb9a1d140a73e591d7ebedda14fd228ae
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-23 20:48:04 -0800

    Time: 19 ms (37.15%) | Memory: 19.2 MB (90.64%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,42 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        #down or right
+        m = len(grid)
+        n = len(grid[0])
+        dprow = [float('inf')] * n   # no manual start cell
+        for i in range(m):
+            newrow = [0]*n
+            for j in range(n):
+                if i == 0 and j == 0:
+                    newrow[j] = grid[0][0]
+                else:
+                    topcell = dprow[j] if i > 0 else float('inf') #read from dprow not grid
+                    leftcell = newrow[j-1] if j >0 else float('inf') #read fromnewrow
+                    newrow[j] = min(topcell,leftcell)+grid[i][j]
+            dprow = newrow
+            print(dprow)
+
+        return dprow[-1]
+
+#
+# Using 0 there makes “no path” look better than any real path, so the DP never accumulates cost along the first row / first column.
+
+#
+#   for row in range(len(grid)):
+#             for col in range(len(grid[0])):
+#                 if row == 0 and col != 0:
+#                     grid[row][col] += grid[row][col - 1]
+#                 elif col == 0 and row != 0:
+#                     grid[row][col] += grid[row - 1][col]
+#                 elif row != 0 and col != 0:
+#                     grid[row][col] += min(grid[row - 1][col], grid[row][col - 1])
+#         return grid[-1][-1]
+
+
+
+
+
+
+
+
+        
