ef09c435f8a568f362966ffb4238188aa12788bf
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-23 20:49:49 -0800

    Time: 11 ms (91.67%) | Memory: 19.3 MB (90.64%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -3,25 +3,25 @@ class Solution:
         #down or right
         m = len(grid)
         n = len(grid[0])
-        dprow = [float('inf')] * n   # no manual start cell
+        dprow = [float('inf')] * n   # use inf not zero!!
         for i in range(m):
             newrow = [0]*n
             for j in range(n):
-                if i == 0 and j == 0:
+                if i == 0 and j == 0: #this necessary case
                     newrow[j] = grid[0][0]
                 else:
                     topcell = dprow[j] if i > 0 else float('inf') #read from dprow not grid
-                    leftcell = newrow[j-1] if j >0 else float('inf') #read fromnewrow
+                    leftcell = newrow[j-1] if j >0 else float('inf') #read from newrow
                     newrow[j] = min(topcell,leftcell)+grid[i][j]
             dprow = newrow
             print(dprow)
 
         return dprow[-1]
 
-#
+#topcell = dprow[j] if i > 0 else float('inf') --- USE INF NOT ZERO!!!
 # Using 0 there makes “no path” look better than any real path, so the DP never accumulates cost along the first row / first column.
 
-#
+#similar answer without any space, from deepti video
 #   for row in range(len(grid)):
 #             for col in range(len(grid[0])):
 #                 if row == 0 and col != 0:
