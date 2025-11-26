974d8b0715003ca81bec746321d6921be2165106
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-26 13:51:59 -0800

    Time: 123 ms (16.45%) | Memory: 23.4 MB (88.24%) - LeetSync

--- 525-contiguous-array/contiguous-array.py
+++ 525-contiguous-array/contiguous-array.py
@@ -2,14 +2,14 @@ class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
         maxlen = 0
         prefixsum = {} #store, sum: index
-        prefixsum[0] = -1 #So that a subarray starting at index 0 is counted.
+        prefixsum[0] = -1 #IMPP- So that a subarray starting at index 0 is counted.
         currsum = 0
         for i in range(len(nums)):
             num = 1 if nums[i] == 1 else -1
             currsum += num
             if currsum in prefixsum: #means that sum has been seen before
                 index = prefixsum[currsum]
-                subarraysize = i - index
+                subarraysize = i - index #this
                 maxlen = max(maxlen, subarraysize)
 
             #store prefix sum
@@ -17,10 +17,15 @@ class Solution:
                 prefixsum[currsum] = i
         return maxlen
 
+#Intuition, Think of prefix sum as your altitude
+# - Equal 0s and 1s means the sum returns to the same level as before
+# - If at two different times (indices), your altitude is the same, then the height gained and height lost between those points must cancel out.
 
-        
-#n2 subarray for n elements
-#cant use sliding window, cause its not possible to make a guarranted decision of when to shrink
+##Little tricky to understand by i+1 to j --
+#If prefix[i] == prefix[j]
+#sum of the subarray from index i to j? prefix[j] – prefix[i-1]
+# Prefix sums measure everything before the index, so the difference between equal prefix sums always corresponds to the subarray after i up to j:
+# (i+1 to j).
 
 #Idea,Interesting!!!
 # - turn all 0 to -1s
@@ -28,6 +33,9 @@ class Solution:
 # - Maintain a hashmap storing the first index where each prefix sum appears.
 # - If the prefix sum repeats at index j (first seen at i), then the subarray (i+1 … j) has sum 0 → equal 0s and 1s.
 # - Initialize prefix sum 0 at index –1 so segments starting at index 0 are counted.
-
-#Try to visualise, increase when +1, decrease when -1
+#Try to visualise that sum increase when +1, decrease when -1
 # If the prefix sum ever returns to a value you’ve seen before, it means everything between those two positions canceled out to zero.
+
+        
+#n2 subarrays for n elements
+#cant use sliding window, cause its not possible to make a guarranted decision of when to shrink
