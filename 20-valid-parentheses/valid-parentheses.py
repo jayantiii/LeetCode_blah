6c73971966fe870d1be6f613b43c755a8d65add4
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-11-25 21:08:36 -0800

    Time: 0 ms (100.00%) | Memory: 18 MB (36.69%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,22 +1,16 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        #O(n),O(n)
         stack = []
-        dic = { "}":"{", "]":"[", ")":"(" }
-        for i in range(len(s)):
-            if s[i] in dic: #took much time to think and form the if else block
-                if stack and stack[-1] == dic[s[i]]:
-                    stack.pop()
-                else:
-                    return False      
+        hashmap = { "(":")", "[":"]", "{":"}"}
+        for x in s:
+            if x in hashmap: #open bracket
+                stack.append(x)
             else:
-                stack.append(s[i])
-        return stack == [] #or  return True if not stack else False
+                if not stack: return False
+                open = stack.pop()
+                if hashmap[open] != x:
+                    return False
+
+        return True if not stack else False
 
     
-#brute force, O(n2),O(n)
-# while '()' in s or '{}' in s or '[]' in s:
-#             s = s.replace('()', '')
-#             s = s.replace('{}', '')
-#             s = s.replace('[]', '')
-#         return s == ''
