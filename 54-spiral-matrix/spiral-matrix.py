19f914a3ad59594e298b247e2351a9db56b9ad74
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-03 08:12:47 -0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (62.08%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,35 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        res = []
+        while matrix:
+            #first row
+            firstrow = matrix.pop(0) 
+            for x in firstrow:
+                res.append(x)
+
+            #last col
+            if not matrix or not matrix[0]:break
+            for row in matrix:
+                lastcol = row.pop(-1)
+                res.append(lastcol)
+            
+            if not matrix or not matrix[0]:break
+            #lastrow, but in reverse!!
+            lastrow = matrix.pop(-1)
+            for x in range(len(lastrow)-1,-1,-1): #reverse ok!
+                res.append(lastrow[x])
+            
+            if not matrix or not matrix[0]:break
+            #firstcol in reverse!!
+            for row in matrix[::-1]:
+                firstcol = row.pop(0)
+                res.append(firstcol)
+
+        return res
+
+
+#remove the respected rows and columns as we traverse
+#We can then make while loop of repeated such steps
+
+        
