6ca7d0cc35d00aed86a3db04453e1e8a1e2e7715
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-10 08:50:57 -0800

    Time: 0 ms (100.00%) | Memory: 17.7 MB (52.83%) - LeetSync

---
+++ 2704-maximum-difference-by-remapping-a-digit/maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        num = str(num)#we cant loop over string
+        firstnonine, firstnonzero ='9','0' #initialiase as string!!
+        for i in range(len(num)):
+            if num[i] != '9':
+                firstnonine = num[i]
+                break
+        for i in range(len(num)):
+            if num[i] !='0':
+                firstnonzero = num[i]
+                break
+
+        maxnum =int(num.replace(firstnonine, '9'))
+        minnum = int(num.replace(firstnonzero, '0'))
+
+        return maxnum - minnum
+
+#Try to remap the first non-nine digit to 9 to obtain the maximum number.
+#Try to remap the first non-zero digit to 0 to obtain the minimum number.
+
+#  maxnum = num is wrong, if we do this, they both point at same
+# maxnum[i] = '9' # also wrong, string cant mutate
