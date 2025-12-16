cc1802d46481522eb03c8efa1ca117dbbd476b71
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-16 14:05:07 -0800

    Time: 4 ms (6.00%) | Memory: 18.5 MB (25.88%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,8 +1,8 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        res = [str(i) for i in range(n+1)]
+        res = [str(i) for i in range(n+1)] #str needed
         for i in range(n+1):
-            if i % 3 == 0 and i % 5 == 0:
+            if i % 3 == 0 and i % 5 == 0:  #start with this case
                 res[i] = "FizzBuzz"
             elif i%3 == 0:
                 res[i] = "Fizz"
@@ -10,3 +10,4 @@ class Solution:
                  res[i] = "Buzz"
 
         return res[1:]
+
