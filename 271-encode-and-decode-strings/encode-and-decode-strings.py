d2bd66e262bfb56d5d6d1cbdf16de4894e7da4a5
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-16 16:33:46 -0800

    Time: 79 ms (24.93%) | Memory: 18.3 MB (40.12%) - LeetSync

--- 271-encode-and-decode-strings/encode-and-decode-strings.py
+++ 271-encode-and-decode-strings/encode-and-decode-strings.py
@@ -16,11 +16,11 @@ class Codec:
         while i < len(s):
             # read full number (can be multi-digit)
             j = i
-            while s[j] != "#":
+            while s[j] != "#": #while needed
                 j+=1
             num = int(s[i:j]) #not j+1
 
-            word = s[j+1:j+1 +num]
+            word = s[j+1:j+1 +num] # understand the j+1 +num
             res.append(word)
             i = j+ num + 1
 
@@ -36,6 +36,6 @@ class Codec:
 # "Hello","World" -- > "5#Hello5#World" 
 #we do like this because if we only use # this to seperate what if a word has # this in it
 
-#Mymistake, handles only 1-digit lengths also dont do s[i].isdigit() cause what if a word has a digit!!
+#Mymistake!!!, handles only 1-digit lengths also dont do s[i].isdigit() cause what if a word has a digit!!
 #  if s[i].isdigit() and s[i+1] == "#": - wrong
 #This only works if the length is like 3#.... If the length is 12#..., then s[i+1] is '2', not '#', so the condition fails and you never decode anything.
