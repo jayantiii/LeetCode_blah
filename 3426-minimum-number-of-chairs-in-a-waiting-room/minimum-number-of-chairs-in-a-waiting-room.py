74c42931711e1a4daa39966b72070d9742366e4d
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-18 04:05:08 -0800

    Time: 0 ms (100.00%) | Memory: 17.1 MB (100.00%) - LeetSync

--- 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.py
+++ 3426-minimum-number-of-chairs-in-a-waiting-room/minimum-number-of-chairs-in-a-waiting-room.py
@@ -12,15 +12,19 @@ class Solution:
         return minchairs
 
 #Main idea - use people variable and not just chair like below       
-
 #My first answer!! - one logic flaw, cant just simply -=1
 #cause if we have used a chair means we add to count, cant just do -=1
 
-        # minchair = 0
-        # for i in range(len(s)):
-        #     if s[i] == "E":
-        #         minchair +=1
-        #     else:
-        #         minchair -=1
-        # return minchair
+# minchair = 0
+# for i in range(len(s)):
+#     if s[i] == "E":
+#         minchair +=1
+#     else:
+#         minchair -=1
+# return minchair
         
+
+#Another way using stack!!
+# 'E' = push (a person enters)
+# 'L' = pop (a person leaves)
+# chairs needed = maximum stack size at any time
