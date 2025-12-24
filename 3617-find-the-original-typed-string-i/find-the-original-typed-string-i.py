123386bc3e248f56563b6e426d90970347ffaf58
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-24 15:59:07 -0800

    Time: 36 ms (72.10%) | Memory: 17.2 MB (97.53%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -1,22 +1,19 @@
 class Solution:
     def possibleStringCount(self, word: str) -> int:
         prevchar = word[0]
-        ways = 0
+        ways = 1 # for original
         for i in range(1,len(word)):
             if word[i] == prevchar:
                 ways+=1
             prevchar = word[i]
 
-        return ways + 1
-
-
-
-
-
+        return ways
 
 
 #keyword -  done this at most once and number of possible original strings 
-
-#My first ans, wrong, i didnt get the question
+#-------My first ans, wrong, i didnt get the question------------------
 #       return len(set(word)) - This is wrong 
 #exampke - "abbcccc", output gives 3 but answer is 5
+
+#---------------------Why no DP / backtracking--------------------
+# DP/backtracking is for when choices interact across positions (one choice changes what’s possible later). Here, “at most once” makes the structure additive: you either choose which run was affected (independent) and how much to shrink it. That collapses into a simple sum.
