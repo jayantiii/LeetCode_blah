d423ba1fdf9073d5d1fdd47fe05f45933012ae4f
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-29 19:46:55 -0800

    Time: 0 ms (100.00%) | Memory: 17.3 MB (93.99%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -7,11 +7,12 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List
     [int]]:
+        #O(n),O(n)
         if not root: #edgecase
             return []
         q = deque([(root,0)]) #(node,column) 
         columns = {} #{colnumber:arrayofnodes}
-        numberofcolumns = 0
+        numberofcolumns = 0 # or can store min and max cols vars
         result = []
         while q:
             size = len(q)
@@ -38,6 +39,13 @@ class Solution:
 # Do BFS from the root. Let the root be at column 0. In the BFS, keep in the queue the node and its column.
 
 # When you traverse a node, store its value in the column index. For example, the root's value should be stored at index 0.
+
 # If the node has a left node, it column should be col - 1. Similarly, if the node has a right node, its column should be col + 1.
 
 # At the end, check the minimum and maximum col and output their values.
+
+#------------Time--------
+#why is it not O(n2) even if while and for nest there?
+# --count work per node, not loops in isolation
+# --Each node triggers one iteration of processing in BFS.
+# No step iterates over all nodes as a result of another per-node step.
