161143bb95914d28be7e1d434ab06f152259357f
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2025-12-29 19:34:44 -0800

    Time: 0 ms (100.00%) | Memory: 17.2 MB (97.15%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -0,0 +1,43 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List
+    [int]]:
+        if not root:
+            return []
+        q = deque([(root,0)]) #(node,column) 
+        columns = {} #{colnumber:arrayofnodes}
+        numberofcolumns = 0
+        result = []
+        while q:
+            size = len(q)
+            for i in range(size):
+                node, col = q.popleft()
+
+                if col not in columns:
+                    columns[col] = []
+                    numberofcolumns+=1
+                columns[col].append(node.val)
+                
+                if node.left:
+                    q.append((node.left, col-1))
+                if node.right:
+                    q.append((node.right, col+1))
+
+        startcol = min(columns.keys())
+        for  i in range(startcol, startcol +  numberofcolumns):
+            col = columns[i]
+            result.append(col)
+        return result
+
+# HINTS
+# Do BFS from the root. Let the root be at column 0. In the BFS, keep in the queue the node and its column.
+
+# When you traverse a node, store its value in the column index. For example, the root's value should be stored at index 0.
+# If the node has a left node, it column should be col - 1. Similarly, if the node has a right node, its column should be col + 1.
+
+# At the end, check the minimum and maximum col and output their values.
