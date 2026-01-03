6701fcad19451f1bd8734ead5ecbb6404daf23c8
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-03 06:05:30 -0800

    Time: 8 ms (74.38%) | Memory: 24.7 MB (77.22%) - LeetSync

--- 359-logger-rate-limiter/logger-rate-limiter.py
+++ 359-logger-rate-limiter/logger-rate-limiter.py
@@ -3,7 +3,6 @@ class Logger:
     def __init__(self):
         self.hashmap = {}
         
-
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
         if message not in self.hashmap:
             self.hashmap[message] = timestamp
@@ -14,7 +13,6 @@ class Logger:
                 return True
         return False
         
-
 
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
