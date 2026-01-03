b5202ed5b7ac0fa9bf9eba1ef8b7e5d3a73fce9d
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-02 16:57:10 -0800

    Time: 7 ms (61.79%) | Memory: 17.8 MB (86.96%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.py
@@ -0,0 +1,63 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        prefixsum = {0:1} 
+        self.count = 0
+        def dfs(node,currsum):
+            if not node:
+                return
+
+            currsum = currsum + node.val
+             # count paths ending at this node
+            self.count += prefixsum.get(currsum - targetSum, 0)
+
+            # add current prefix sum to the path
+            prefixsum[currsum] = prefixsum.get(currsum, 0) + 1
+
+            # recurse (DO NOT add node.val again)
+            dfs(node.left, currsum)
+            dfs(node.right, currsum)
+
+            # backtrack
+            prefixsum[currsum] -= 1
+
+        dfs(root,0)
+
+        return self.count
+        
+
+
+#use prefix sum and hashmap
+# A downward path ending at current node sums to target if there exists an earlier prefix sum currSum - target.
+# Keep a dict cnt[prefixSum] = how many times this prefix sum has appeared on the current root→node pat
+
+# You can’t prune with (currSum + node.val) > targetSum because node values can be negative.
+
+# You’re not handling node is None.
+
+# You’re not using the prefix-sum idea correctly: you need a count map of prefix sums along the current root→node path, with backtracking.
+
+# Variable name mismatches (currsum vs currSum, target vs targetSum).
+
+        # prefixsum = {0: 1}
+
+        # def dfs(node, currsum):
+        #     if not node:
+        #         return 0
+
+        #     currsum += node.val
+        #     count = prefixsum.get(currsum - targetSum, 0)
+
+        #     prefixsum[currsum] = prefixsum.get(currsum, 0) + 1
+        #     count += dfs(node.left, currsum)
+        #     count += dfs(node.right, currsum)
+        #     prefixsum[currsum] -= 1  # backtrack
+
+        #     return count
+
+        # return dfs(root, 0)
