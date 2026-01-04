0e09740b16d8a6500f6e25c5a5ce2b3644cf236f
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-04 03:12:31 -0800

    Time: 370 ms (28.25%) | Memory: 18.4 MB (99.69%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -9,3 +9,15 @@ class Solution:
 
 
 # Tip - > do not delete anything from the string cuz it will lead to TLE, build another string instead
+#------------------one more way, use list----------------------------------
+    #    res = []
+    #     for c in s:
+    #         if len(res) >= 2 and res[-1] == c and res[-2] == c: ##check from result string!
+    #             continue
+    #         res.append(c)
+    #     return ''.join(res)
+
+##-----------------------Why “don’t delete from the string”:---------------------
+# Python strings are immutable. Every “delete” or concatenation like ans += c creates a new string 
+# → repeated copies → can degrade to O(n²) time.
+#List append is amortized O(1), and ''.join(res) is O(n) total.
