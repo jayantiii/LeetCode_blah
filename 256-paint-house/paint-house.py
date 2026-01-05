e50693f2a593e6e3afd8d3684e44bf6b64bde748
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-04 16:23:09 -0800

    Time: 0 ms (100.00%) | Memory: 17.6 MB (75.41%) - LeetSync

--- 256-paint-house/paint-house.py
+++ 256-paint-house/paint-house.py
@@ -7,6 +7,7 @@ class Solution:
     # h2.  18 35 7
     # h3   21. 10 
 
+        # Bottom up approach
         prevhouse = [0,0,0]
  
         for i in range(0,len(costs)):
@@ -19,11 +20,33 @@ class Solution:
         return min(prevhouse)
 
 
+        #BRUTEFORCE - You start from house i and try every allowed colour. For each colour, you recursively compute the cost of painting the next house with the constraint that no two adjacent houses share a colour.
+# Time complexity = O(3^n) because for every house you have 3 choices.
+        # n = len(costs)
+
+        # # dfs(i, prev) returns the minimum cost from house i to end
+        # # given that previous house was painted with color 'prev'
+        # def dfs(i, prev):
+        #     # Base case: painted all houses
+        #     if i == n:
+        #         return 0
+
+        #     best = float('inf')
+
+        #     # Try all 3 colours
+        #     for color in range(3):
+        #         # Can't use same colour as previous house
+        #         if color != prev:
+        #             cost_now = costs[i][color]
+        #             # Recursively compute cost of painting next houses
+        #             total_cost = cost_now + dfs(i + 1, color)
+        #             best = min(best, total_cost)
+
+        #     return best
+
+        # # Start at house 0 with no previous color
+        # return dfs(0, -1)
         
-
-
-        
-
 #I started this try but it wont lead anywhere much
 #    n = len(costs)
 #         if !costs: return 0
