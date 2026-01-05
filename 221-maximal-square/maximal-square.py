57e9c24d47792250286f7409661b877999dfec97
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-04 17:35:00 -0800

    Time: 372 ms (14.85%) | Memory: 106.8 MB (8.59%) - LeetSync

--- 221-maximal-square/maximal-square.py
+++ 221-maximal-square/maximal-square.py
@@ -27,3 +27,46 @@ class Solution:
 
 #No visited set needed      
 # In this problem, revisiting a cell is not a bug; recomputing the same dfs(i,j) repeatedly is the bug. Memoization fixes that, visited breaks correctness or forces you into awkward logic.
+
+##-------------------Bottom up DP!----------------------------
+# dp[m,n] , max square side length whose bottom-right corner is at m,n
+    #   if not matrix or not matrix[0]:
+    #         return 0
+
+    #     R, C = len(matrix), len(matrix[0])
+    #     dp = [[0] * (C + 1) for _ in range(R + 1)]
+
+    #     best_side = 0
+    #     for i in range(1, R + 1):
+    #         for j in range(1, C + 1):
+    #             if matrix[i - 1][j - 1] == "1":
+    #                 dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+    #                 best_side = max(best_side, dp[i][j])
+
+    #     return best_side * best_side
+
+#if we wanna doo top left, corner wise, then fill table backward from bottom
+# --------------Example matrix (recursion)-------------------------:
+# 1 1 1 1
+# 1 1 0 1
+# 1 1 1 1
+# 1 1 1 1
+#
+# dfs(i,j) = max SQUARE SIDE starting at (i,j)
+
+# Key cell: dfs(0,0)
+# down  = dfs(1,0) = 2   (from row1,col0 you can make a 2x2 of ones)
+# right = dfs(0,1) = 1   (from row0,col1 you CANNOT make 2x2 because (1,2)=0 blocks)
+# diag  = dfs(1,1) = 1   (same reason, any 2x2 starting at (1,1) hits (1,2)=0)
+#
+# So:
+# dfs(0,0) = 1 + min(down,right,diag)
+#          = 1 + min(2,1,1)
+#          = 2
+#
+# Intuition of "min":
+# To grow a kxk square at (i,j), you need:
+# - (k-1)x(k-1) square at (i+1,j)   (supports everything below top row)
+# - (k-1)x(k-1) square at (i,j+1)   (supports everything right of left col)
+# - (k-1)x(k-1) square at (i+1,j+1) (supports the interior)
+# The weakest of these 3 limits how big you can grow => min(...)
