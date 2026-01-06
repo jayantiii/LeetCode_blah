f44ea4c6fa4b2d859f99808664e263f069745766
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-06 11:12:07 -0800

    Time: 5 ms (59.75%) | Memory: 19.4 MB (18.65%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.py
+++ 299-bulls-and-cows/bulls-and-cows.py
@@ -16,8 +16,32 @@ class Solution:
                 s[guess[i]] -=1
         return str(bulls) + "A" + str(cows) + "B"
 
+#My major mistake - I tried counting bulls and cows in one pass(oneloop)------>
 # The problem: a digit in guess can match a digit in secret either as a cow or as a bull, but bulls must take priority. With your one-pass cow counting, you can “spend” a digit as a cow, and then later you reach the position where that digit should have been a bull — and you still count the bull too. That overcounts cows.
 # core flaw: counting cows using only secret’s counts in a single left-to-right pass doesn’t enforce bull priority across future positions.
+
+##------------------------- Other soln, no hashing, one pass-------------------------------
+# Traverse both strings simultaneously.
+# If characters match exactly, count it as a bull.
+# If not, count their occurrences in separate frequency arrays.
+# After traversal, count cows as the minimum of corresponding digit counts.
+
+
+#         bulls = cows = 0
+#         freq_secret = [0] * 10
+#         freq_guess = [0] * 10
+
+#         for s, g in zip(secret, guess):
+#             if s == g:
+#                 bulls += 1
+#             else:
+#                 freq_secret[int(s)] += 1
+#                 freq_guess[int(g)] += 1
+
+#         for i in range(10):
+#             cows += min(freq_secret[i], freq_guess[i])
+
+#         return f"{bulls}A{cows}B"
 
 
 
