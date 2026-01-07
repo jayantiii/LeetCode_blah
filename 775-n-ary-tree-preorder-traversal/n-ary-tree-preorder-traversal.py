f0f4037c5e3368e686da13529bdbcbcb0841f91c
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-07 14:58:09 -0800

    Time: 65 ms (7.14%) | Memory: 21.2 MB (14.96%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.py
@@ -0,0 +1,9 @@
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        
+        result = [root.val]
+        for child in root.children:
+            result.extend(self.preorder(child))
+        return result
