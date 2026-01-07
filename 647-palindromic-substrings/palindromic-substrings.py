d492cb394b4d6e908f65fcca79f09ba34446f8a1
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-07 11:56:15 -0800

    Time: 191 ms (31.70%) | Memory: 27.2 MB (6.74%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.py
+++ 647-palindromic-substrings/palindromic-substrings.py
@@ -33,7 +33,10 @@ class Solution:
 
 # (lower triangle l>r is unused)
 
-#-------------- Why (j - i <= 1)?------------
+# if (j-i)<=1 or dp[i+1][j-1]: 
+#the order matters (j-i)<=1 before , acts as boundary check for i+1, j-1
+
+#-------------- Why (j - i <= 1)?-----------------------
 # dp[i][j] wants to look "inside" at dp[i+1][j-1].
 # But for very short substrings, there is no real "inside":
 #   - length 1 (j==i): inside is dp[i+1][i-1]  -> invalid/empty
@@ -41,8 +44,25 @@ class Solution:
 # So for length 1 or 2, if the end chars match, it's automatically a palindrome.
 # That's exactly what (j - i <= 1) checks: length = (j - i + 1) is 1 or 2.
 
-
-#----------------------Why we dont need dp size n+1 * n+1---------------------
+#----------------------Why we dont need dp size n+1 * n+1 but n*n-----------------
 # # “Substrings” here means non-empty contiguous pieces of s.
 # So you do not count the empty string.
 # In the DP code, that’s why we start with length = 1 (not 0).
+
+#----------------------------Exapnd around center method--------------------
+#    def countSubstrings(self, s: str) -> int:
+#         n = len(s)
+
+#         def expand(l: int, r: int) -> int:
+#             cnt = 0
+#             while l >= 0 and r < n and s[l] == s[r]:
+#                 cnt += 1
+#                 l -= 1
+#                 r += 1
+#             return cnt
+
+#         ans = 0
+#         for c in range(n):
+#             ans += expand(c, c)       # odd length
+#             ans += expand(c, c + 1)   # even length
+#         return ans
