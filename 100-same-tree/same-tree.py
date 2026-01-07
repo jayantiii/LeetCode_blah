3723d1c2966a5e205eb8faf26aaed43799ee6acd
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-07 14:33:46 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (14.19%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -18,4 +18,14 @@ class Solution:
             return left and right
         else:
             return False
+
+
+#clean!
+        # if not p and not q:
+        #     return True
+        
+        # if p and q and p.val == q.val:
+        #     return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        # return False
         
