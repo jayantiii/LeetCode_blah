78b193d442ca752e8fa3fc966df58d9921b62aed
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-08 12:00:28 -0800

    Time: 11 ms (17.27%) | Memory: 20.8 MB (11.86%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -2,15 +2,42 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         m = len(triangle)
         n = len(triangle[0])
-        @lru_cache(None)
-        def backtrack(i,j):
-            if i < 0 or i >= m or j <0 or j >= i + 1: # cond j >= i + 1
-                return 0
+ 
+        #dp[i][j] is min path some ending at i,j
+        # Outer loop first, then inner loop
+        dp = [[float("inf") for j in range(i+1)] for i in range(m)]
+        dp[0][0] = triangle[0][0]
 
-            first = backtrack(i+1, j)
-            second = backtrack(i+1,j+1)
+        for i in range(1,m):
+            for j in range(0,i+1):
+                # from directly above (i-1, j) exists only when j <= i-1  <=> j < i
+                above = float("inf")   
+                left  = float("inf")   
+                if  j < i: #THIS!
+                    above = min(dp[i][j], dp[i-1][j])
 
-            return triangle[i][j] + min(first,second)
+                # from above-left (i-1, j-1)
+                if  j-1 >=0:
+                    left =  min(dp[i][j],dp[i-1][j-1])
 
-        return backtrack(0,0)
+                dp[i][j] =  triangle[i][j] + min(above,left)
+
+        return min(dp[m-1]) #max in the last row
+
+
+#BACKTRACKING!!
+        # m = len(triangle)
+        # n = len(triangle[0])
+
+        # @lru_cache(None)
+        # def backtrack(i,j): #return min pathsum of cell (i,j)
+        #     if i < 0 or i >= m or j <0 or j >= i + 1: # cond j >= i + 1
+        #         return 0
+
+        #     first = backtrack(i+1, j)
+        #     second = backtrack(i+1,j+1)
+
+        #     return triangle[i][j] + min(first,second)
+
+        # return backtrack(0,0)
         
