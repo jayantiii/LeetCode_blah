6ac2e3b94368dde87b2f254c9d5b1ff05e3fc2ee
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-10 09:21:09 -0800

    Time: 55 ms (73.36%) | Memory: 25.3 MB (5.68%) - LeetSync

---
+++ 1165-meeting-scheduler/meeting-scheduler.py
@@ -0,0 +1,45 @@
+class Solution:
+    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
+        slots1.sort()
+        slots2.sort()
+
+        j = 0
+        for s1,e1 in slots1:
+            # skip slots2 that end before s1 starts
+            while j < len(slots2) and slots2[j][1] < s1:
+                j += 1
+
+            # try overlapping slots2 starting from current j
+            while j < len(slots2) and slots2[j][0] <= e1:
+
+                s2, e2 = slots2[j][0], slots2[j][1]
+
+                commontime = min(e1,e2) - max(s1,s2)
+                if commontime >= duration: #found ans
+                    #find the range
+                    start = max(s1,s2)
+                    end = start + duration
+                    return [start,end]
+            
+                # IMP! # discard the interval that ends earlier
+                if e2 <= e1:
+                    j += 1 #          # slots2 ends first -> move j
+                else:
+                    # slots1 ends first -> next s1
+                    break
+                        
+
+        return []
+
+
+
+
+
+#important to note that the slots are AVAILABILITY times of each person. I originally misinterpreted it as meeting times and was trying to find a gap between the slots instead of an intersection.
+
+#What if there are n slots? How would you solve it?
+
+
+ 
+
+ 
