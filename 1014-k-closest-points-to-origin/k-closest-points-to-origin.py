3a6600cb2ee088a4830e6e113ef550d2f448e278
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-10 07:51:57 -0800

    Time: 67 ms (60.74%) | Memory: 25.5 MB (5.32%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -14,4 +14,42 @@ class Solution:
 
         return res
 
+# can use  heapq.heapify(heap) too!
+
+#----------Better Binary Search Time complexity: O(N)---------------------------------
+# Binary search isn’t on the array order here. It’s on the answer value (the distance threshold T).
+# Idea: find a distance threshold T such that at least k points have d <= T, but fewer than k have d < T. Then:
+
+# take all points with d < T
+# add enough points with d == T to reach k
+# This works because distances are monotonic w.r.t. the threshold.
+#max (hi): max(x*x + y*y for all points)
+
+#----------------Quickselect apporach----------------------------
+# Quickselect (partition like Quicksort, but only recurse into one side)
+# Idea: partition points around a pivot distance so that:
+# left side = distances < pivot
+# right side = distances >= pivot
+# Then only continue on the side that contains the k-th element. Finally, points[:k] are the k closest (order arbitrary).
+
+#-------------------Bruteforce---------------------------------------
+#Bruetforce Approach: Sort with Custom Comparator, O(N⋅logN)
+#Python, on the other hand, uses TimSort, which is a hybrid of MergeSort and InsertionSort and requires O(N) extra space.
+
+#--------------------Using maxheap when we want heap size k-----------------------
+# heap = []  # max-heap via (-dist2, x, y)
+
+#         for x, y in points:
+#             dist2 = x*x + y*y
+#             item = (-dist2, x, y)
+
+#             if len(heap) < k:
+#                 heapq.heappush(heap, item)
+#             else:
+#                 heapq.heappushpop(heap, item)  # keeps k closest
+
+#         return [[x, y] for _, x, y in heap]
+
+
+
         
