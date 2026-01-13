9506e58834ea115390f382f716da76b38da6e548
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-13 15:17:02 -0800

    Time: 47 ms (39.50%) | Memory: 51.2 MB (5.07%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.py
@@ -27,21 +27,35 @@ class Solution:
                 dfs(i)
                 numofcomp +=1
         return numofcomp - 1
-        
+
+# Intuition / Idea
+# - Treat the network as an undirected graph with:
+# - Minimum edges needed to connect n nodes is (n - 1).
+#   So if m < n - 1 -> impossible -> return -1.
+
+# - Otherwise, we have enough cables in total. We just need to know how many
+#   disconnected groups (connected components) there are.
+
+# - If there are c connected components, we need exactly (c - 1) cables to connect them.
+
+# We can always “reuse” extra cables from cycles, so we don’t need to find the exact rewiring. # Answer = c - 1
+
+# Complexity:
+# - Build graph + DFS over all nodes/edges: Time O(n + m)
+# - Graph + visited: Space O(n + m)   (visited alone is O(n))
 
 
+#--------------EXTRA - How to find num of redundant edges!!!-------------------
+# Let:
+#   n = number of nodes
+#   m = number of edges
+#   c = number of connected components
+#
+# Minimum edges to keep each component connected (a forest) = n - c
+# Redundant edges (extra / cycle edges) = m - (n - c)
+#
+# Per component:
+#   if a component has k nodes and e edges,
+#   redundant_in_component = e - (k - 1)
 
-# #Observation
-# --> find cycles, cause u can remove 1 from that
-# --> if numofcycles < numofdisconnected components: return -1
-#--> here num of cycle can be considered ar numbe rof reduandant edges
-# --> else return num of dicosnnected comp
-# --> we dont need to find wher eto plug in cables
-# -> Also, if  m < n -1, then all nodes cant be connected directly
-
-# During DFS/BFS over ONE connected component:
-# nodes = number of vertices visited in this component
-# edges = (sum of degrees of visited vertices) // 2
-#         (because in an undirected graph each edge contributes +1 degree to TWO endpoints)
-# redundant_in_component = edges - (nodes - 1)
-#         (a tree on 'nodes' vertices has exactly 'nodes-1' edges; any extra edges must form cycles)
+#this gives the number of redundant edges / independent cycles
