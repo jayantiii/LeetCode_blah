bf3a09f2211714fc7501b41018c05fd1d5f4fd90
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-19 16:41:39 -0800

    Time: 330 ms (38.82%) | Memory: 23 MB (12.28%) - LeetSync

---
+++ 808-number-of-matching-subsequences/number-of-matching-subsequences.py
@@ -0,0 +1,34 @@
+import bisect
+class Solution:
+    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+        hashmap = defaultdict(list)
+        #hashmap of all chars and their indices they present it
+        for i in range(len(s)):
+            letter = s[i]
+            hashmap[letter].append(i)
+
+        def bnysearch(word):
+            previ = -1 #last macthed index in 
+            for i in range(len(word)):
+                char = word[i]
+                idxs = hashmap.get(char,[])
+                j = bisect.bisect_right(idxs,previ) #first index index > prev
+
+                if j ==len(idxs):return False #not found
+                previ = idxs[j]
+            return True
+                
+        count = 0
+        for word in words:
+            if bnysearch(word):
+                count+=1
+
+        return count
+        
+
+#If getting TLE just try reducing your words size by mapping each word with its frequency, and using that maps unique words to check for subsequence instead of actual words arrays.
+
+# HashMap + Binary Search
+# HashMap for storing main string's character index
+# Binary Search for checking each individual word can be created using above hashmap or not
+ 
