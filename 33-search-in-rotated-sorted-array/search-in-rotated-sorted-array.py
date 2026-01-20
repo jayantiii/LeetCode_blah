00b67715af371234d9b84071d810e5ace83ee898
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-01-19 18:05:02 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.85%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -7,19 +7,18 @@ class Solution:
             mid = (l + r)//2
             if nums[mid] == target:
                 return mid
-            if  nums[l] <= nums[mid]: # left sorted portion of array
-                if nums[mid] < target or target < nums[l]: #Imp 
-                    l = mid +1 #how u frame codn in if matters
-                else:
-                    r = mid-1
-            else : #right sorted portion #nums[mid] <= nums[l]
-                if nums[mid] >target or target > nums[r]: ##Imp condn
+
+            if nums[l] <= nums[mid]: #left sorted part
+                if nums[l] <=  target < nums[mid]:
                     r = mid -1
                 else:
-                    l = mid+1
-                
-            # else: # no need here, we need to check target above
-            #     return mid
+                    l = mid +1
+
+            else: #right sorted part
+                if  nums[mid] < target<= nums[r]:
+                    l =mid +1
+                else:
+                    r = mid -1
 
         return -1
         
