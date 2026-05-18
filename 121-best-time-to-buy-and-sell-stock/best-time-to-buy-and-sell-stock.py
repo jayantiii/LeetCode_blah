a4c621117f6d52d439ddcaf0ffaf333f66b17119
Jayanti Lahoti <66568334+jayantiii@users.noreply.github.com>
2026-05-17 17:43:48 -0700

    Time: 71 ms (16.17%) | Memory: 28.7 MB (42.96%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,31 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        res = 0
-        low = prices[0]
-        for price in prices:
-            if price < low:
-                low = price
-            res = max(res, price - low)
-        return res
+        maxp = 0
+        minpurch = prices[0]
+        for i in range(1,len(prices)):
+            currp = prices[i] - minpurch
+            maxp = max(maxp,currp)
+            minpurch = min(minpurch, prices[i])
 
-#can use 2 pointer, try!
-
-# dp!?
-# min_price = float('inf')
-#         max_profit = 0
-#         for current_price in prices:
-#             min_price = min(current_price, min_price)
-#             max_profit = max(max_profit, current_price - min_price)   
-#         return max_profit
-
-        
-
-#brute force - doesnt pass last some cases
-        # max = 0
-        # for i in range(len(prices)):
-        #     for j in range(i+1,len(prices)):
-        #         if prices[j]-prices[i]  > max:
-        #             max = prices[j]-prices[i]
-        # return max
+        return maxp
 
         
